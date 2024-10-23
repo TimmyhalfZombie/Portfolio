b0ebcb96fc74d404c0dac05f0f361d7cd126cb85
Timmy <114127244+TimmyhalfZombie@users.noreply.github.com>
2024-10-23 20:37:48 +0800

    second commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = ({ isMobile }) => {
         object={computer.scene}
         scale={isMobile ? 0.35 : 0.75}
         position={isMobile ? [-6.5, -3.5, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]} // Keep steady rotation for all views
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
